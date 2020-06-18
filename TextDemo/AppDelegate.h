8a42f2318bf8a6633534450e715121610b14f693
“fangtingchao” <ftc0423@163.com>
2020-06-18 16:44:12 +0800

    初始化项目

---
+++ TextDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TextDemo
+//
+//  Created by 方廷超 on 2020/6/18.
+//  Copyright © 2020 方廷超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TextDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TextDemo
+//
+//  Created by 方廷超 on 2020/6/18.
+//  Copyright © 2020 方廷超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
